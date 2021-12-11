ff6fa883b62258ad59270da53e85307ab4bc55ca
Viper Muffin <vipermuffin@gmail.com>
2021-12-10 22:04:07 -0600

    Day 10 refactor

--- Day10/Day10.cxx
+++ Day10/Day10.cxx
@@ -11,20 +11,18 @@
 #include "AoCUtils.h"
 //Common Libraries
 #include <algorithm> //std::sort, find, for_each, max_element, etc
-//#include <array>
 #include <climits>   //INT_MIN, INT_MAX, etc.
 #include <functional> //std::function
 #include <numeric> //std::accumulate
 #include <unordered_map>
 
-
 using namespace std;
 namespace AocDay10 {
-
     static const std::string InputFileName = "Day10.txt";
+    const unordered_map<char, char> matches{{')','('},{']','['},{'}','{'},{'>','<'}};
+
     std::string solvea() {
         auto input = parseFileForLines(InputFileName);
-
 		return to_string(calcSyntaxScore(input));
     }
 
@@ -42,80 +40,60 @@ namespace AocDay10 {
 		return to_string(scores[i]);
     }
 
-char findFirstIllegalChar(const std::string& line) {
-    auto itr = line.begin();
-    vector<char> stack{};
-    unordered_map<char, std::function<bool(char)>> ops{
-        {'(',[&](char c){stack.push_back(c);return false;}},
-        {'{',[&](char c){stack.push_back(c);return false;}},
-        {'[',[&](char c){stack.push_back(c);return false;}},
-        {'<',[&](char c){stack.push_back(c);return false;}},
-        {')',[&](char c){if(stack.back()=='('){stack.pop_back();return false;}return true;}},
-        {'}',[&](char c){if(stack.back()=='{'){stack.pop_back();return false;}return true;}},
-        {']',[&](char c){if(stack.back()=='['){stack.pop_back();return false;}return true;}},
-        {'>',[&](char c){if(stack.back()=='<'){stack.pop_back();return false;}return true;}}
-            };
-    while(itr != line.end()) {
-        if(ops[*itr](*itr)) {
-            return *itr;
+    char findFirstIllegalChar(const std::string& line) {
+        auto itr = line.begin();
+        vector<char> stack{};
+        
+        while(itr != line.end()) {
+            if(matches.count(*itr) == 0) {
+                stack.push_back(*itr);
+            } else {
+                if(stack.back() == matches.at(*itr)) {
+                    stack.pop_back();
+                } else {
+                    return *itr;
+                }
+            }
+            itr++;
         }
-        itr++;
+        return '0';
     }
-    return '0';
-}
 
-int64_t calcSyntaxScore(const std::vector<std::string>& lines) {
-    unordered_map<char, int64_t> values {
-        {'0',0},
-        {')',3},
-        {']',57},
-        {'}',1197},
-        {'>',25137}
-    };
-    vector<int64_t> vals{};
-    vals.reserve(lines.size());
-    for(const auto& line : lines) {
-        vals.push_back(values[findFirstIllegalChar(line)]);
-    }
-    
-    return std::accumulate(vals.begin(), vals.end(), static_cast<int64_t>(0));
-}
-
-int64_t calcIncompleteScore(const std::string& line) {
-    unordered_map<char, int64_t> values {
-        {'(',1},
-        {'[',2},
-        {'{',3},
-        {'<',4}
-    };
-    
-    vector<char> stack{};
-    unordered_map<char, std::function<bool(char)>> ops{
-        {'(',[&](char c){stack.push_back(c);return false;}},
-        {'{',[&](char c){stack.push_back(c);return false;}},
-        {'[',[&](char c){stack.push_back(c);return false;}},
-        {'<',[&](char c){stack.push_back(c);return false;}},
-        {')',[&](char c){if(stack.back()=='('){stack.pop_back();return false;}return true;}},
-        {'}',[&](char c){if(stack.back()=='{'){stack.pop_back();return false;}return true;}},
-        {']',[&](char c){if(stack.back()=='['){stack.pop_back();return false;}return true;}},
-        {'>',[&](char c){if(stack.back()=='<'){stack.pop_back();return false;}return true;}}
-            };
-    
-    auto itr = line.begin();
-    while(itr != line.end()) {
-        if(ops[*itr](*itr)) {
-            return 0;
+    int64_t calcSyntaxScore(const std::vector<std::string>& lines) {
+        unordered_map<char, int64_t> values {{'0',0},{')',3},{']',57},{'}',1197},{'>',25137}};
+        vector<int64_t> vals{};
+        vals.reserve(lines.size());
+        for(const auto& line : lines) {
+            vals.push_back(values[findFirstIllegalChar(line)]);
         }
-        itr++;
+        
+        return std::accumulate(vals.begin(), vals.end(), static_cast<int64_t>(0));
     }
-    int64_t score = 0;
-    auto rItr = stack.rbegin();
-    while(rItr != stack.rend()) {
-        score *= 5;
-        score += values[*rItr];
-        rItr++;
-    }
-    return score;
-}
 
+    int64_t calcIncompleteScore(const std::string& line) {
+        unordered_map<char, int64_t> values {{'(',1},{'[',2},{'{',3},{'<',4}};
+        vector<char> stack{};
+        
+        auto itr = line.begin();
+        while(itr != line.end()) {
+            if(matches.count(*itr) == 0) {
+                stack.push_back(*itr);
+            } else {
+                if(stack.back() == matches.at(*itr)) {
+                    stack.pop_back();
+                } else {
+                    return 0;
+                }
+            }
+            itr++;
+        }
+        
+        int64_t score = 0;
+        auto rItr = stack.rbegin();
+        while(rItr != stack.rend()) {
+            score *= 5;
+            score += values[*rItr++];
+        }
+        return score;
+    }
 }
